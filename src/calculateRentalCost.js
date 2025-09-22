56e5257a393bd0193489f225830aa593a3bb18ba
ivandobychin <ivandobychin123@gmail.com>
2025-09-22 21:08:46 +0300

    fixed bugs

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,12 +5,15 @@
  */
 function calculateRentalCost(days) {
   const totalCost = 40;
-  let sumPay = totalCost * days;
+  const sumPay = totalCost * days;
+
+  const discount7 = 50;
+  const discount3 = 20;
 
   if (days >= 7) {
-    return sumPay - 50;
+    return sumPay - discount7;
   } else if (days >= 3) {
-    return sumPay - 20;
+    return sumPay - discount3;
   }
 
   return sumPay;
